961685615a423899a6320f5543d2ef4b0fbef31a
Fergus Rose <fjmrose@gmail.com>
2022-08-03 13:22:30 +1000

    add build files to app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 app.use(express.json())
-
+app.use(express.static('build'))
 const cors = require('cors')
 app.use(cors())
 
